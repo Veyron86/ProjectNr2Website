b8714d003ab9746ec17fc3bfa94c2ba96e955fef
Piotr <piotrkrol1986@gmail.com>
2023-04-27 06:37:08 +0200

    Add alert for navigation/contact section

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ console.log("Cześć!");
 function myFunction()  {
    let showParagraph = document.querySelector(".close-paragraph");
    showParagraph.classList.toggle("myStyle");
+
+  
 }
 
+// const second = document.getElementById("contact");
+// console.log(second);
+
+function showAlert() {
+   alert("Alert!");
+}
+
+
+//    second.addEventListener("click", () => {
+//       console.log("Section Contact!");
+//    })
+// }
 
